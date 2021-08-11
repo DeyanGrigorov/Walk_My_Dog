5e86645315cd95ef0b478eae0b9cead3b3a5b0b5
Deyan Grigorov <deian1998@abv.bg>
2021-08-11 15:46:48 +0300

    Delete useless func

--- tests/base/mixins.py
+++ tests/base/mixins.py
@@ -10,11 +10,6 @@ class ProfileTestUtils:
     def create_profile(self, **kwargs):
         return Profile.objects.create(**kwargs)
 
-    def create_profile_with_like(self, like_user, **kwargs):
-        profile = self.create_profile(**kwargs)
-        Like.objects.create(profile=profile, user=like_user)
-        return profile
-
 
 class UserTestUtils:
     def create_user(self, **kwargs):
