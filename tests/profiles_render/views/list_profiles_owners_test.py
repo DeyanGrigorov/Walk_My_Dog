7357b4a5f09a1f83abab900330578b67ec06b12d
Deyan Grigorov <deian1998@abv.bg>
2021-08-11 15:49:22 +0300

    modify test for list owners

---
+++ tests/profiles_render/views/list_profiles_owners_test.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.test import Client
+from django.urls import reverse
+
+from tests.base.tests import WalkMyDogTestCase
+
+
+UserModel = get_user_model()
+
+
+class ProfileOwnersList(WalkMyDogTestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = UserModel.objects.create_user(email='deian@abv.bg', password='123456', category='Dog owner')
+
+    def test_getOwnersList_whenLoggedInUser_ShouldSeeListDetailsOwners(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('list profiles owners'))
+
+        profiles = list(response.context['page'])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertListEqual(profiles, list(response.context['profiles']))
+
+
+    # def test_getNoOwnersList_whenLoggedInUser_ShouldSeeNothing(self):
