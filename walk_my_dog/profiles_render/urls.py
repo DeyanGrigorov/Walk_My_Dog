c7317291d8b56a262716eac9be4c87ec4df642ad
Deyan Grigorov <deian1998@abv.bg>
2021-08-12 18:36:58 +0300

    Add search engine

--- walk_my_dog/profiles_render/urls.py
+++ walk_my_dog/profiles_render/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 
-from walk_my_dog.profiles_render.views import list_profiles_owners, list_profiles_sitters, profile_details, like_profile
+from walk_my_dog.profiles_render.views import list_profiles_owners, list_profiles_sitters, profile_details, \
+    like_profile, SearchResultsView
 
 urlpatterns = (
     path('profiles-owners/', list_profiles_owners, name='list profiles owners'),
     path('profiles-sitters/', list_profiles_sitters, name='list profiles sitters'),
     path('profile-details/<int:pk>', profile_details, name='list profile details'),
     path('profile-like/<int:pk>', like_profile, name='like profile'),
+    path('search/', SearchResultsView.as_view(), name='search'),
 
 )
 
--- walk_my_dog/profiles_render/views.py
+++ walk_my_dog/profiles_render/views.py
@@ -1,20 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
 
 from walk_my_dog.profile_auth.models import Profile
 from walk_my_dog.profiles_render.models import Like
 
 
 def list_profiles_owners(request):
-    dog_owners = Profile.objects.filter(category='Dog owner')
+    dog_owners = Profile.objects.filter(category='Dog owner').order_by('user_id')
     dog_owners_paginator = Paginator(dog_owners, 3)
     page_num = request.GET.get('page')
     page = dog_owners_paginator.get_page(page_num)
 
     context = {
         'page': page,
-        'profiles': dog_owners
+        'profiles': dog_owners,
     }
 
     return render(request, 'profiles/list_profiles_owners.html', context)
@@ -60,3 +62,27 @@ def like_profile(request, pk):
         )
         like.save()
     return redirect('list profile details', pk)
+
+
+class SearchResultsView(ListView):
+    model = Profile
+    template_name = 'profiles/search_results.html'
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query_first_name'] = self.request.GET.get('q_first_name')
+        context['query_category'] = self.request.GET.get('q_category')
+        return context
+
+    def get_queryset(self):
+        query_first_name = self.request.GET.get('q_first_name')
+        query_category = self.request.GET.get('q_category')
+
+        object_list = Profile.objects.filter(
+            Q(first_name__icontains=query_first_name), Q(category__icontains=query_category)
+        ).order_by('user_id')
+
+        return object_list
+
+
