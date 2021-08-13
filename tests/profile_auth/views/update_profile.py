8cddc4c28e2791afd6854c0bd82d36b6385a18fb
Deyan Grigorov <deian1998@abv.bg>
2021-08-13 17:20:14 +0300

    Nothing changed

--- tests/profile_auth/views/update_profile.py
+++ tests/profile_auth/views/update_profile.py
@@ -6,17 +6,18 @@ from tests.base.tests import WalkMyDogTestCase
 
 class ProfileUpdateTest(WalkMyDogTestCase, UserTestUtils, ProfileTestUtils):
     def test_profile_expected_fields(self):
-        user = self.create_user(email='deni@abv.bg', password='12345678', first_name='Deyan')
+        user = self.create_user(email='test@abv.bg', password='12345678', first_name='Test')
 
         expected_profile_title = f'{user.first_name}'
         expected_profile_email = f'{user.email}'
-        self.assertEquals(expected_profile_title, 'Deyan')
-        self.assertEquals(expected_profile_email, 'deni@abv.bg')
+        self.assertEquals(expected_profile_title, 'Test')
+        self.assertEquals(expected_profile_email, 'test@abv.bg')
 
     def test_Update(self):
-        user = self.create_user(email='deni@abv.bg', password='12345678', first_name='Deyan')
+        user = self.create_user(email='test@abv.bg', password='12345678', first_name='Test')
         user.first_name = "This has been changed"
         expected_profile_first_name = f'{user.first_name}'
         self.assertEquals(expected_profile_first_name, 'This has been changed')
+
 
 
