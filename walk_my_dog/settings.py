d8528a8e3dc527376c6db19711d3dbd29322bf09
Deyan Grigorov <deian1998@abv.bg>
2022-04-08 22:46:20 +0300

    deployment settings made

--- walk_my_dog/settings.py
+++ walk_my_dog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5(sm$nd#3nbg*li2rv2y(*6pnr-s9np(0#=p(=@)q)@rmvw+)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['walk_my_dog.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
